bda0c7864636f093a2e4db4696633b2e850f51ac
Jamie Patel <jamiepatel@Jamies-MacBook-Pro.local>
2014-06-18 11:11:00 +0100

    User can sign in

--- app/server.rb
+++ app/server.rb
@@ -44,6 +44,35 @@ post '/users' do
 	else
 		flash.now[:errors] = @user.errors.full_messages
 		erb :"users/new"
-		# redirect '/users/new'
 	end
 end
+
+get '/sessions/new' do 
+	erb :"sessions/new"
+end
+
+post '/sessions' do 
+	email, password = params[:email], params[:password]
+	user = User.authenticate(email, password)
+	if user
+		session[:user_id] = user.id
+		redirect '/'
+	else
+		flash[:errors] = ["The email or password is incorrect"]
+		erb :"sessions/new"
+	end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+

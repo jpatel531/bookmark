95dcfaf041693ff3f0c68ff815c87208d5c3cfbc
jpatel531 <jamie@notespublication.com>
2014-08-20 19:52:11 +0100

    About to push to github

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -19,3 +19,33 @@ delete '/sessions' do
 	session[:user_id] = nil
 	redirect '/'
 end
+
+post "/sessions/forgotten" do 
+	user = User.first(email: params[:femail])
+	user.password_token = (1..64).map{("A".."Z").to_a.sample}.join
+	user.password_token_timestamp = Time.now
+	user.save
+	redirect '/sessions/forgotten/email-sent'
+end
+
+get '/sessions/forgotten/email-sent' do 
+	"An email with a unique token has been sent to your account"
+end
+
+
+get "/sessions/reset_password/:token" do 
+	@user = User.first(password_token: params[:token])
+	if Time.now - @user.password_token_timestamp <= (3600)
+		erb :reset_password
+	else
+		"You have taken too long to reset your password"
+	end
+end
+
+post "/sessions/reset_password/:token/new_password" do 
+	user = User.first(password_token: params[:token])
+	user.password = params[:reset_password]
+	user.password_token = nil
+	user.save
+	redirect '/'
+end
--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,4 +1,5 @@
 env = ENV["RACK_ENV"] || "development"
 
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+DataMapper.auto_upgrade!
 DataMapper.finalize
